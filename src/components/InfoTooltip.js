957d4659c881e90264b263014cd5eab31d021035
Maca <maca@brutalrocks.com>
2023-06-21 00:07:12 -0400

    listo para enviar

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -9,19 +9,21 @@ import errorIcon from "../images/error.png"
 export default function InfoTooltip(props) {
 
   return (
+
     <Popup open={props.open} handleClose={props.handleClose}>
       <div className="popup">
         <img className="popup__successIcon"
-            src={props.stateInfoToolTip ? successIcon : errorIcon}
-            alt="exito"
-          />
-          <h1 className="popup__successTitle">
-            {props.stateInfoToolTip
-              ? "¡Correcto! Ya estás registrado."
-              : "Uy, algo salió mal. Por favor, inténtalo de nuevo."}{" "}
-          </h1>
+          src={props.stateInfoToolTip ? successIcon : errorIcon}
+          alt="exito"
+        />
+        <h1 className="popup__successTitle">
+          {props.stateInfoToolTip
+            ? "¡Correcto! Ya estás registrado."
+            : "Uy, algo salió mal. Por favor, inténtalo de nuevo."}{" "}
+        </h1>
       </div>
     </Popup>
   );
 }
+
 
